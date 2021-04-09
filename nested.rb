4fc92a6761b6a26e9af792c6e7c4c14e3e9b5c5d
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-09 16:23:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -53,8 +53,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-language_as_string = programmer_hash[:dennis_ritchie][:languages]
-language_as_string
+programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
