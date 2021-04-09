a199acd06a8041f3d8c6e288a3e0f1032d10c8dd
Christopher Black <christopher.black.2209@googlemail.com>
2021-04-09 16:34:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -92,8 +92,6 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
 programmer_hash[:alan_kay][:known_for] = "GUI"     
 programmer_hash     
 end
@@ -114,5 +112,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
